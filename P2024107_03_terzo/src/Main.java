d928f81a5b4fc13cbcf6e3fe888bc6651a7b2d07
marimarafa <arafamarim166@gmail.com>
2024-10-07 13:58:41 +0200

    ..

---
+++ P2024107_03_terzo/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		double d1 = 45.3;
+		double d2 = 67.2;
+		//Math.sqrt();
+		System.out.println("La lunghezza dell'ipotenusa: " );
+
+	}
+
+}

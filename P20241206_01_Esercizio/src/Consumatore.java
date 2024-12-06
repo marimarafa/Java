74e27f358b814a6a2d1a16d471cfce78da6370d5
marimarafa <arafamarim166@gmail.com>
2024-12-06 07:01:33 +0100

    ..

---
+++ P20241206_01_Esercizio/src/Consumatore.java
@@ -0,0 +1,17 @@
+public class Consumatore {
+
+	public Consumatore() {
+		super();
+	}
+
+	static double numero =0.0;
+	public static void RiceviCarattere(char c) {
+		if(c == '\n') {
+			System.out.println("Il numero è :" + numero );
+		} else {
+			
+			
+		}
+		
+	}
+}
---
+++ P20241206_01_Esercizio/src/Produttore.java
@@ -0,0 +1,13 @@
+public class Produttore {
+
+	public Produttore() {
+		super();
+	}
+
+	public static void ScandireStringa(String stringa) {
+		for(int i=0; i> stringa.length(); i++) {
+			char car = stringa.charAt(i);
+			Consumatore.RiceviCarattere(car);
+		}
+	}
+}

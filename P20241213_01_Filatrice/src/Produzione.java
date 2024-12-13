f6c83f19fbf7ab83b0423330829aaa88a703cfd2
marimarafa <arafamarim166@gmail.com>
2024-12-13 13:35:42 +0100

    ..

---
+++ P20241213_01_Filatrice/src/Produzione.java
@@ -0,0 +1,13 @@
+class Produzione{
+	 public String prodotto;
+	 public int metraggio;
+	 
+	 public Produzione(String prodotto, int metraggio) {
+		 super();
+		 this.prodotto = prodotto;
+		 this.metraggio = metraggio;
+	 }
+	 public Produzione() {
+		 super();
+	 }
+}

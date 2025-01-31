ac3629c509689eb59672ae8fae3f0d2ba4250d0c
marimarafa <arafamarim166@gmail.com>
2025-01-31 18:49:31 +0100

    ..

---
+++ P20250131_01_UnitTest/Test/SommaTest.java
@@ -0,0 +1,11 @@
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class SommaTest {
+
+	@Test
+	void testSomma() {
+		assertEquals(2, Calcolatrice.Somma(1, 1) );
+	}
+}
---
+++ P20250131_01_UnitTest/src/Somma.java
@@ -0,0 +1,12 @@
+public class Somma {
+	public static void main(String[] args) {
+		Somma(2, 1);
+	}
+
+
+
+	public static int Somma(int i, int j) {
+	return i + j;
+
+	}
+}
